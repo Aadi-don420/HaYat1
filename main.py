7f337c1c6745bc31bc46acc116996add15471677
Aadi-don420 <pankaj849095@gmail.com>
2025-09-02 02:17:16 +0530

    main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> HAYAT MULTI CONVO</title>
+  <title> AADI MULTI CONVO</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -131,7 +131,7 @@ def send_message():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mt-3">HAYAT MULTI CONVO</h1>
+    <h1 class="mt-3"> AADI MULTI CONVO</h1>
   </header>
   <div class="container text-center">
     <form method="post" enctype="multipart/form-data">
@@ -181,12 +181,12 @@ def send_message():
     </form>
   </div>
   <footer class="footer">
-    <p>Â© 2023 CODED BY :- HAYAT</p>
-    <p> HAYAT FUCKING MASTER </a></p>
+    <p>Â© 2023 CODED BY :- AADI</p>
+    <p> AADI FUCKING MASTER </a></p>
     <div class="mb-3">
-      <a href="https://wa.me/+923321516644" class="whatsapp-link">
+      <a href="https://wa.me/+919173534581" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i><button>CHAT ON WP</button>
-        <a href="https://www.facebook.com/Hayat143j?mibextid=ZbWKwL"><button>CHAT ON FB</button>
+        <a href="https://www.facebook.com/Legend203729"><button>CHAT ON FB</button>
         </a>
     </div>
   </footer>
